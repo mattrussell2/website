f92c5ae055f89a97e5c40c989915e67f5fd033c7
Matt Russell <mrussell@cs.tufts.edu>
2023-02-21 14:30:10 -0500

    webpack fix, and make text box sizes bigger

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     new CopyWebpackPlugin({
       'patterns':[
         { from: 'src/assets', to: 'assets' }, 
-        { from: 'CNAME', to: '.'}
+        "CNAME"
       ]})
   ]
 };
