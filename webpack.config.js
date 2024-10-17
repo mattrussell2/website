cbb2ccd675a74b660446cafe0a35a2f346113d9d
Matt Russell <mrussell@cs.tufts.edu>
2024-10-17 09:24:09 -0400

    mp3 compatible webpack and easy dev environment

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,60 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(mp3|wav)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/'
+            }
+          }
+        ]
+      }
+      {
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: [
+          'raw-loader',
+          'glslify-loader'
+        ]
+      }
+    ]
+  },
   plugins: [
     new CopyWebpackPlugin({
       'patterns':[
         { from: 'src/assets', to: 'assets' }, 
         { from: 'src/CNAME', toType: 'file', to: 'CNAME'}
       ]})
-  ]
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+  mode: 'development',
+  devtool: 'eval-source-map',
+  resolve: {
+    alias: {
+      'three': path.resolve('./node_modules/three')
+    }
+  }
 };
